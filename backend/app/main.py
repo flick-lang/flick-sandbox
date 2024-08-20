28efb80ac1bf065b6a7414cbd0a08f4fc3600316
Max Larsson <maxslarsson@gmail.com>
2024-08-19 23:54:38 -0400

    Implement basic websocket communication

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.websocket("/ws/compiler")
+async def compiler(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            flick_source = await websocket.receive_text()
+
+            # compile the source
+
+            # send compiler output
+
+            # run the source
+
+            for _ in range(3):
+                await websocket.send_text(f"Message text was: {flick_source}\n")
+    except WebSocketDisconnect:
+        pass
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("Submitted!")
-    
+  const [socket, setSocket] = useState(null)
+  const [source, setSource] = useState("")
+  const [output, setOutput] = useState("")
+  
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/ws/compiler")
+    setSocket(ws)
+
+    ws.onmessage = (event) => {
+      setOutput((prevOutput) => prevOutput + event.data)
+    }
+
+    return () => {
+      ws.close()
+    }
+  }, [])
+
+
+  const compileCode = (e) => {
+    if (socket.readyState !== WebSocket.OPEN) {
+      setOutput("Failed to connect to compiler")
+      return
+    }
+
+    setOutput("Compiling...\n")
+    socket.send(source)
   }
 
   return (
     <div className="App">
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <textarea id="flick-source" name="flick-source" placeholder="fn main() i32 {" rows="5"></textarea>
-        <input type="submit"></input>
-      </form>
-      <p id="flick-output">This is the text output</p>
+      <textarea onChange={(e) => setSource(e.target.value)}></textarea>
+      <button onClick={compileCode}>Compile</button>
+      <p>{output}</p>
     </div>
   );
 }
