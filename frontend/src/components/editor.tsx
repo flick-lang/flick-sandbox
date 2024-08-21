64c522e7493f77d2596daa6dbb82074d3728cd83
Max Larsson <maxslarsson@gmail.com>
2024-08-21 18:32:29 -0400

    Add syntax highlighting

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -8,7 +8,47 @@ import { Button } from './ui/button';
 
 
 export default function Editor() {
-    const handleEditorWillMount = (monaco: Monaco) => {}
+    const handleEditorWillMount = (monaco: Monaco) => {
+        monaco.languages.register({ id: 'flick' });
+
+        // Register a tokens provider for the language
+        // see: https://microsoft.github.io/monaco-editor/monarch.html
+        monaco.languages.setMonarchTokensProvider('flick', {
+            // Set defaultToken to invalid to see what you do not tokenize yet
+            // defaultToken: 'invalid',
+
+            keywords: [
+                "pub", "fn"
+            ],
+            typeKeywords: [
+                "void"
+            ],
+            symbols:  /[=><!+\-*\/]+/,
+            operators: [
+                '=', '==', '!=', '>', '>=', '<', '<=', '/', '/=', '*', '*=', '+', '+=', '-', '-='
+            ],
+
+            tokenizer: {
+                root: [
+                    [/[a-z_$][\w$]*/, {
+                        cases: {
+                            '@typeKeywords': 'keyword',
+                            '@keywords': 'keyword',
+                            '@default': 'identifier'
+                        }
+                    }],
+                    [/[A-Z][\w\$]*/, 'type.identifier'],  // to show class names nicely
+
+                    [/@symbols/, {
+                        cases: {
+                            '@operators': 'operator',
+                            '@default': ''
+                        }
+                    }],
+                ]
+            }
+        });
+    }
 
     const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
         // Disable features for simplicity
@@ -24,7 +64,7 @@ export default function Editor() {
                     Run
                 </Button>
             </div>
-            <MonacoEditor defaultValue="// some comment" beforeMount={handleEditorWillMount} onMount={handleEditorDidMount} />
+            <MonacoEditor defaultValue="// some comment" defaultLanguage="flick" beforeMount={handleEditorWillMount} onMount={handleEditorDidMount} />
         </div>
     )
 }
