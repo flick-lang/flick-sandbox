7ed20c8e1bbb7b1ff134c50ae513245e009d2bd6
Max Larsson <maxslarsson@gmail.com>
2024-08-21 18:19:29 -0400

    Simplify editor options

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -13,28 +13,7 @@ export default function Editor() {
     const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
         // Disable features for simplicity
         editor.updateOptions({
-            // Basic setup
             minimap: { enabled: false }, // Disable minimap
-            lineNumbers: 'on', // Show line numbers
-            folding: false, // Disable code folding
-            wordWrap: 'off', // Disable word wrapping
-            scrollBeyondLastLine: true, // ALlow scrolling beyond last line
-            readOnly: false, // Allow editing
-            
-            // Disable advanced features
-            automaticLayout: true,
-            quickSuggestions: false, // Disable suggestions
-            parameterHints: { enabled: false }, // Disable parameter hints
-            suggestOnTriggerCharacters: false, // Disable suggestions on typing trigger characters
-            snippetSuggestions: 'none', // Disable snippet suggestions
-            tabCompletion: 'off', // Disable tab completion
-            hover: { enabled: false }, // Disable hover tooltips
-            contextmenu: false, // Disable right-click context menu
-            lightbulb: { enabled: "off" }, // Disable code actions
-            cursorBlinking: 'solid', // Simpler cursor style
-            renderIndentGuides: true, // Render indent guides
-            renderLineHighlight: 'none', // Disable line highlight
-            renderValidationDecorations: 'off', // Disable validation decorations
         });
     }
 
