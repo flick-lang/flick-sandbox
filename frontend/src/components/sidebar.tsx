c23f61fef2d350fb64984a8dc0e39395751b5167
Thomas Breydo <tbreydo@gmail.com>
2024-09-16 00:17:49 -0400

    Use better CSS practices; mention i11

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,14 +1,9 @@
 export default function Sidebar() {
   return (
-    <div className="flex flex-col h-full p-6 overflow-y-auto bg-gray-50">
-      <h1 className="font-bold text-2xl mb-4">The Flick Sandbox</h1>
-      {/* <section className="mb-6">
-        <p className="text-sm mb-2 italic">
-          A coding environment for Flick.
-        </p>
-      </section> */}
-      
-      <section className="mb-6">
+    <div className="flex gap-6 flex-col min-h-full p-6 bg-gray-50">
+      <h1 className="font-bold text-2xl">The Flick Sandbox</h1>
+
+      <section>
         <h2 className="font-semibold text-xl mb-2">Getting Started</h2>
         <ol className="list-decimal list-inside text-sm space-y-2">
           <li>Choose an example from the dropdown menu.</li>
@@ -17,23 +12,22 @@ export default function Sidebar() {
         </ol>
       </section>
       
-      <section className="mb-6">
+      <section>
         <h2 className="font-semibold text-xl mb-2">About Flick</h2>
         <p className="text-sm mb-2">
           We created the Flick programming language to explore compiler design. Learn more <a
            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
             href="https://github.com/flick-lang/flick">here</a>.
         </p>
-
       </section>
       
-      <section className="mb-6">
+      <section>
         <h2 className="font-semibold text-xl mb-2">Key Language Features</h2>
-        <ul className="list-disc list-inside text-sm">
+        <ul className="list-disc list-inside text-sm space-y-2">
           <li>Static typing with type inference</li>
           <li>Functions and external function declarations</li>
           <li>Simple control flow (if/else, while loops)</li>
-          <li>Variable-sized integers (u8, i32, etc.)</li>
+          <li>Variable-sized integers (u8, i11, etc.)</li>
         </ul>
       </section>
       
